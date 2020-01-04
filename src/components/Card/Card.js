72611387581d4ba9de33645ca797f715a35e91c2
florinrinja <florinrinja@outlook.com>
2020-01-04 18:34:39 +0100

    update Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,11 +3,11 @@ import React from 'react'
 import classes from './Card.module.css';
 
 const card = (props) => {
-    return(
-        <div className={classes.Card}>
-            {props.children}
-        </div>
-    );
+  return (
+    <div className={classes.Card}>
+      {props.children}
+    </div>
+  );
 }
 
 export default card;
